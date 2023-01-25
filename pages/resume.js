a6e4da2e5967540cb38fd41434d1ad3b219e112f
Jarret Coyle <jarretcoyletoo@gmail.com>
2023-01-24 17:29:37 -0700

    fixed anonymous function issue

--- pages/resume.js
+++ pages/resume.js
@@ -2,7 +2,7 @@ import Head from "next/head"
 import Script from 'next/script'
 import Layout from '../components/layout'
 
-export default function () {
+export default function Resume() {
 return(  
   <Layout>
     <Head>
