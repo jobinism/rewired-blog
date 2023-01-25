31fba3d5ab76da30ce76a5a664eb5cd990473c35
Jarret Coyle <jarretcoyletoo@gmail.com>
2023-01-24 17:22:11 -0700

    retool resume iframe

--- pages/resume.js
+++ pages/resume.js
@@ -2,14 +2,14 @@ import Head from "next/head"
 import Script from 'next/script'
 import Layout from '../components/layout'
 
-export default function Resume() {
+export default function () {
 return(  
   <Layout>
     <Head>
       <title>Projects</title>
     </Head>
     <iframe src="https://resume.creddle.io/embed/8hw1okrxhxe"
-    width={1200} height={1200} seamless>
+    width="850" height="1100" seamless>
 
     </iframe>
   </Layout>)
