8a9cc7d045db191fee3d07eac7c34e185501410b
Jarret Coyle <jarretcoyletoo@gmail.com>
2023-09-19 21:11:58 -0600

    adjusted size of audio player project

--- pages/projects.js
+++ pages/projects.js
@@ -25,7 +25,7 @@ export default function FirstPost() {
     <div className="row">
         <iframe
         src="https://albumrelease-df77nazrc-jobinism.vercel.app/"
-        height={600}
+        height={800}
         width={600}
         seamless
         ></iframe>
